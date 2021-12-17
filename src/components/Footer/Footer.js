676453ef33b673f8ec9282e9d16365945beecb1d
Waynexia888 <qiwuwaynexia@gmail.com>
2021-12-17 13:35:27 -0800

    added header and footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div> 
+      Copyright &copy; {new Date().getFullYear()} EventList
+    </div>
+  );
+}
--- src/components/app.js
+++ src/components/app.js
@@ -4,14 +4,20 @@ import { Switch } from 'react-router-dom';
 
 import LoginContainer from './session/login_container';
 import SignupContainer from './session/signup_container';
+import NavBar from './navbar/navbar';
+import Footer from './Footer/Footer';
 
 const App = () => {
     return(
         <div>
+            <NavBar />
             <Switch>
+                {/* <AuthRoute exact path="/" component={MainPage} /> */}
                 <AuthRoute exact path="/login" component={LoginContainer} />
                 <AuthRoute exact path="/signup" component={SignupContainer} />
             </Switch>
+            <Footer />
+
         </div>
     )
 }
